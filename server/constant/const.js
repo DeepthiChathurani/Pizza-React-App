5b29e002082e633ae097dc1d42d9c04e40ad5ee3
deepthichathurani64@gmail.com <deepthichathurani64@gmail.com>
2024-06-12 11:55:04 +0530

    first commit

--- server/constant/const.js
+++ server/constant/const.js
@@ -1,25 +1,189 @@
 const productList=[
     {
         "id": 1,
-        "name": "spinach",
-        "price": 200,
+        "name": "Chilli Chicken Pizza",
+        "price": 800,
         "currency": "LKR",
-        "image": "spinach.png"
+        "image": "Chilli Chicken Pizza.png"
     },
     {
         "id": 2,
-        "name": "Tomato",
-        "price": 300,
+        "name": "Cheesy Onion with Green Chiliese",
+        "price": 800,
         "currency": "LKR",
-        "image": "tomato.jpeg"
+        "image": "Cheesy Onion with Green Chiliese.png"
     },
     {
         "id": 3,
-        "name": "Beans",
-        "price": 500,
+        "name": "Cheesy Tomato with Green Chilliese",
+        "price": 800,
         "currency": "LKR",
-        "image": "beans.jpeg"
+        "image": "Cheesy Tomato with Green Chilliese.png"
+    },
+
+    {
+        "id": 4,
+        "name": "Chicken Bacon & Potato with Nai Miris",
+        "price": 800,
+        "currency": "LKR",
+        "image": "Chicken Bacon & Potato with nai miris.png"
+    },
+    {
+        "id": 5,
+        "name": "Sausage Delight",
+        "price": 800,
+        "currency": "LKR",
+        "image": "Sausage Delight.png"
+    },
+    {
+        "id": 6,
+        "name": "Double Chicken Surprise",
+        "price": 950,
+        "currency": "LKR",
+        "image": "Double Chicken Surprise.png"
+    },
+    {
+        "id": 7,
+        "name": "Cheese Lovers",
+        "price": 950,
+        "currency": "LKR",
+        "image": "Cheese Lovers.png"
+    },
+    {
+        "id": 8,
+        "name": "Tandoori Chicken Pizza",
+        "price": 950,
+        "currency": "LKR",
+        "image": "Tandoori Chicken pizza.png"
+    },
+    {
+        "id": 9,
+        "name": "Hot & Spicy Chicken Pizza",
+        "price": 950,
+        "currency": "LKR",
+        "image": "Hot & Spicy Chicken pizza.png"
+    },
+    {
+        "id": 10,
+        "name": "Black Chicken Pizza",
+        "price": 950,
+        "currency": "LKR",
+        "image": "black chicken pizza.png"
+    },
+    {
+        "id": 11,
+        "name": "Spicy Veggie with Paneer Pizza",
+        "price": 1140,
+        "currency": "LKR",
+        "image": "Spicy Veggie with Paneer pizza.png"
+    },
+    {
+        "id": 12,
+        "name": "Seafood Treat",
+        "price": 1240,
+        "currency": "LKR",
+        "image": "Seafood Treat.png"
+    },
+    {
+        "id": 13,
+        "name": "Devilled Chicken Pizza",
+        "price": 1140,
+        "currency": "LKR",
+        "image": "Devilled Chicken pizza.png"
+    },
+    {
+        "id": 14,
+        "name": "BBQ Chicken Pizza",
+        "price": 1140,
+        "currency": "LKR",
+        "image": "BBQ Chicken pizza.png"
+    },
+    {
+        "id": 15,
+        "name": "Chicken Hawaiian Pizza",
+        "price": 1140,
+        "currency": "LKR",
+        "image": "Chicken Hawaiian pizza.png"
+    },
+    {
+        "id": 16,
+        "name": "Butter Chicken Masala Pizza",
+        "price": 1140,
+        "currency": "LKR",
+        "image": "Butter Chicken Masala pizza.png"
+    },
+    {
+        "id": 17,
+        "name": "Double Chicken & Cheese Fiesta Pizza",
+        "price": 1140,
+        "currency": "LKR",
+        "image": "Double Chicken & Cheese Fiesta pizza.png"
+    },
+    {
+        "id": 18,
+        "name": "Sri Lankan Spicy Chicken Pizza",
+        "price": 1260,
+        "currency": "LKR",
+        "image": "Sri Lankan Spicy Chicken Pizza.png"
+    },
+    {
+        "id": 19,
+        "name": "Prawn with Chicken Bacon & Jalapeno Pizza",
+        "price": 1260,
+        "currency": "LKR",
+        "image": "Prawn with Chicken Bacon & Jalapeno pizza.png"
+    },
+    {
+        "id": 20,
+        "name": "Veggie Supreme Pizza",
+        "price": 1260,
+        "currency": "LKR",
+        "image": "Veggie Supreme pizza.png"
+    },
+    {
+        "id": 21,
+        "name": "Beef Pepperoni Pizza",
+        "price": 1260,
+        "currency": "LKR",
+        "image": "Beef Pepperoni pizza.png"
+    },
+    {
+        "id": 22,
+        "name": "Meat Lovers-Chicken Pizza",
+        "price": 1260,
+        "currency": "LKR",
+        "image": "Meat Lovers - Chicken pizza.png"
+    },
+    {
+        "id": 23,
+        "name": "Meat Lovers-Beef Pizza",
+        "price": 1260,
+        "currency": "LKR",
+        "image": "Meat Lovers - Beef pizza.png"
+    },
+    {
+        "id": 24,
+        "name": "The Ultimate Chicken Treat Pizza",
+        "price": 1560,
+        "currency": "LKR",
+        "image": "The Ultimate Chicken Treat pizza.png"
+    },
+    {
+        "id": 25,
+        "name": "Super Supreme Pizza",
+        "price": 1560,
+        "currency": "LKR",
+        "image": "Super Supreme pizza.png"
+    },
+    {
+        "id": 26,
+        "name": "Hot Garlic Prawns Pizza",
+        "price": 1560,
+        "currency": "LKR",
+        "image": "Hot Garlic Prawns pizza.png"
     }
+
+
 ];
 const test=[];
 /*obj widiyt dunne product.js wladi puluwn oder ekk nthuw denn*/
